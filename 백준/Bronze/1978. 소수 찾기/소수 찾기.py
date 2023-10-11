34015a72843504583377097a23f6cb91ccb3625b
Hwang_yerin <50723984+Hwangyerin@users.noreply.github.com>
2023-10-11 10:27:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,12 +1,12 @@
 n = int(input())
 num_list = map(int, input().split())
-cnt = 0
-sum1 = 0
-for i in num_list:
-    cnt = 0
-    for j in range(1,i+1):
-        if i%j == 0:
-            cnt += 1
-    if cnt == 2:
-        sum1 += 1
-print(sum1)
+answer = 0
+for j in num_list:
+    L = []
+    for i in range(1,int(j**0.5)+1):
+        if j%i == 0:
+            L.append(i)
+            L.append(j//i)
+    if len(set(L)) == 2:
+        answer += 1
+print(answer)
