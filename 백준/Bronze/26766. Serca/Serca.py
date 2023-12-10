06df7c75e3311b56de3768502dec835add57d6d9
Hwang_yerin <50723984+Hwangyerin@users.noreply.github.com>
2023-12-10 22:50:10 +0900

    [Bronze V] Title: Serca, Time: 472 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,13 @@
+n =int(input())
+
+
+for i in range(n):
+    print(' @@@   @@@ ') 
+    print('@   @ @   @')
+    print('@    @    @')
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")   
+    print("    @ @    ")   
+    print("     @     ")    
