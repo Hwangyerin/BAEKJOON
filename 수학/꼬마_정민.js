1c55e6af21f4f17759dc88d1241aa32b96762dfe
Hwangyerin <lucialu02@naver.com>
2025-09-10 09:16:27 +0900

    꼬마 정민

--- 수학/꼬마_정민.js
+++ 수학/꼬마_정민.js
@@ -4,13 +4,3 @@ const b = Number(input[1]);
 const c = Number(input[2]);
 const result = a + b + c;
 console.log(result);
-
-
-
-/*
-const input = require('fs').readFileSync('example.txt').toString().split(' ');
-const a = Number(input[0]);
-const b = Number(input[1]);
-const result = a - b;
-console.log(result);
-*/
